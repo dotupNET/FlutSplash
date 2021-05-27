4400a89b1aa7af741ffe6f6326874d15bef0e3e4
Peter Ullrich <dotup.software@gmail.com>
2021-05-27 17:06:51 +0200

    working

---
+++ android/app/src/main/kotlin/com/yashgarg/flutsplash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yashgarg.flutsplash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/helpers/keys.dart
+++ lib/helpers/keys.dart
@@ -1,4 +1,5 @@
 class Keys {
   // Your application’s access key
-  static String UNSPLASH_API_CLIENT_ID = '';
+  static String UNSPLASH_API_CLIENT_ID = 'HIYglA2g-hfJ_Gf1BnTN92Sj0i6qR8B2ZGYUtAfgr-8';
+  static String UNSPLASH_API_SECRET_ID = '_eTPAhYvovflS5t-xc1o7oMjafg-rd4ubPUOpxrnKGk';
 }
